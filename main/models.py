4fd2a53d14c9bd5328d860f6f8ff8a3716ace665
Ketan Sahu <ketankumar120@gmail.com>
2020-07-30 20:52:50 +0530

    database connected to user

--- main/models.py
+++ main/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Contact(models.Model):
     email = models.EmailField(max_length=200)
     linkedin = models.URLField(max_length=200)
     github = models.URLField(max_length=200)
     phone = models.PositiveIntegerField()
+    user = models.ForeignKey(User, default=None, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.email
@@ -13,21 +15,21 @@ class Education(models.Model):
     institute = models.CharField(max_length=200)
     passed = models.DateField()
     score = models.CharField(max_length=100)
-    email = models.ForeignKey(Contact, default=1, on_delete=models.SET_DEFAULT)
+    user = models.ForeignKey(User, default=None, on_delete=models.CASCADE)
     def __str__(self):
         return self.institute
 
 class Experience(models.Model):
     role = models.CharField(max_length=200)
     detail = models.TextField(max_length=1000)
-    email = models.ForeignKey(Contact, default=1, on_delete=models.SET_DEFAULT)
+    user = models.ForeignKey(User, default=None, on_delete=models.CASCADE)
     def __str__(self):
         return self.role
 
 class Skill(models.Model):
     skill = models.CharField(max_length=200)
     strength = models.CharField(max_length=200)
-    email = models.ForeignKey(Contact, default=1, on_delete=models.SET_DEFAULT)
+    user = models.ForeignKey(User, default=None, on_delete=models.CASCADE)
     def __str__(self):
         return self.skill
     
